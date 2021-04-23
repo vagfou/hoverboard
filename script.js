2acef203668601d0a36d3a70365d89e429ca82a9
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-23 19:29:12 +0300

    project done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 500
+const SQUARES = 2000
 
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
@@ -20,7 +20,8 @@ function setColor(element) {
 }
 
 function removeColor(element) {
-
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
 }
 
 function getRandomColor() {
