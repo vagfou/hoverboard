df0f14e6f2ee688d0b5c7001c3c55043a5cf06ee
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-23 19:22:36 +0300

    squares draw function done

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    container.appendChild(square)
+}
