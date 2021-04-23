b45d7d15930303ad45e36b9b07a37ff5d09d1dd1
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-23 19:27:05 +0300

    color on hover done

--- script.js
+++ script.js
@@ -6,5 +6,23 @@ for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
     container.appendChild(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
 }
